d73f1a8c97b63af1119835808a7226a8044b279e
MD Maruf <maruf84602@gmail.com>
2026-04-24 12:40:04 +0600

    Made person class

---
+++ Bank_management/Employee.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_management
+{
+    class Employee : Person
+    {
+
+        public int EmployeeId
+        {
+            get;
+            set;
+        }
+
+        public string Position
+        {
+            get;
+            set;
+        }
+        public double Salary
+        {
+            set;
+            get;
+        }
+        public int BranchId
+        {
+            get;
+            set;
+
+        }
+
+        public Employee(string name,string email,string position,int phone,double salary,int branchId) : base(name, email, phone)
+        {
+            this.Position = position;
+            this.Salary = salary;
+            this.BranchId = branchId;
+
+        }
+
+    }
+}
