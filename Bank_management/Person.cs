3d341d292efbe501a420f40f27ed0ca63b5b82eb
MD Maruf <maruf84602@gmail.com>
2026-04-24 19:27:54 +0600

    NOthing

--- Bank_management/Person.cs
+++ Bank_management/Person.cs
@@ -8,5 +8,16 @@ namespace Bank_management
 {
     class Person
     {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Phone { get; set; }
+
+        public Person(string name,string email,int phone)
+        {
+            this.Name = name;
+            this.Email = email;
+            this.Phone = phone;
+        }
+
     }
 }
