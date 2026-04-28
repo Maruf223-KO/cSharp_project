45751d4316c0ce76b9d7b2dc448680ae4bc5d34e
MD Maruf <maruf84602@gmail.com>
2026-04-28 21:08:52 +0600

    added Customer form

---
+++ Bank_management/CustomerRegister.Designer.cs
@@ -0,0 +1,273 @@
+﻿
+namespace Bank_management
+{
+    partial class CustomerRegister
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.customerId = new System.Windows.Forms.Label();
+            this.txt_CustomerId = new System.Windows.Forms.TextBox();
+            this.lable_Name = new System.Windows.Forms.Label();
+            this.txt_Name = new System.Windows.Forms.TextBox();
+            this.label_Phone = new System.Windows.Forms.Label();
+            this.txt_Phone = new System.Windows.Forms.TextBox();
+            this.label_Email = new System.Windows.Forms.Label();
+            this.txt_Email = new System.Windows.Forms.TextBox();
+            this.label_Sex = new System.Windows.Forms.Label();
+            this.label_Address = new System.Windows.Forms.Label();
+            this.txt_Address = new System.Windows.Forms.TextBox();
+            this.label_Branch = new System.Windows.Forms.Label();
+            this.txt_Branch = new System.Windows.Forms.TextBox();
+            this.combox_Sex = new System.Windows.Forms.ComboBox();
+            this.label_Title = new System.Windows.Forms.Label();
+            this.btn_Submit = new System.Windows.Forms.Button();
+            this.btn_Clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // customerId
+            // 
+            this.customerId.AutoSize = true;
+            this.customerId.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.customerId.Location = new System.Drawing.Point(12, 148);
+            this.customerId.Name = "customerId";
+            this.customerId.Size = new System.Drawing.Size(145, 25);
+            this.customerId.TabIndex = 0;
+            this.customerId.Text = "Customer ID :";
+            // 
+            // txt_CustomerId
+            // 
+            this.txt_CustomerId.Cursor = System.Windows.Forms.Cursors.No;
+            this.txt_CustomerId.Location = new System.Drawing.Point(163, 149);
+            this.txt_CustomerId.Multiline = true;
+            this.txt_CustomerId.Name = "txt_CustomerId";
+            this.txt_CustomerId.ReadOnly = true;
+            this.txt_CustomerId.Size = new System.Drawing.Size(149, 24);
+            this.txt_CustomerId.TabIndex = 1;
+            // 
+            // lable_Name
+            // 
+            this.lable_Name.AutoSize = true;
+            this.lable_Name.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lable_Name.Location = new System.Drawing.Point(70, 220);
+            this.lable_Name.Name = "lable_Name";
+            this.lable_Name.Size = new System.Drawing.Size(87, 25);
+            this.lable_Name.TabIndex = 2;
+            this.lable_Name.Text = "Name : ";
+            // 
+            // txt_Name
+            // 
+            this.txt_Name.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.txt_Name.Location = new System.Drawing.Point(163, 221);
+            this.txt_Name.Multiline = true;
+            this.txt_Name.Name = "txt_Name";
+            this.txt_Name.Size = new System.Drawing.Size(149, 24);
+            this.txt_Name.TabIndex = 3;
+            // 
+            // label_Phone
+            // 
+            this.label_Phone.AutoSize = true;
+            this.label_Phone.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Phone.Location = new System.Drawing.Point(561, 145);
+            this.label_Phone.Name = "label_Phone";
+            this.label_Phone.Size = new System.Drawing.Size(93, 25);
+            this.label_Phone.TabIndex = 4;
+            this.label_Phone.Text = "Phone : ";
+            // 
+            // txt_Phone
+            // 
+            this.txt_Phone.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.txt_Phone.Location = new System.Drawing.Point(671, 146);
+            this.txt_Phone.Multiline = true;
+            this.txt_Phone.Name = "txt_Phone";
+            this.txt_Phone.Size = new System.Drawing.Size(149, 24);
+            this.txt_Phone.TabIndex = 5;
+            // 
+            // label_Email
+            // 
+            this.label_Email.AutoSize = true;
+            this.label_Email.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Email.Location = new System.Drawing.Point(570, 217);
+            this.label_Email.Name = "label_Email";
+            this.label_Email.Size = new System.Drawing.Size(84, 25);
+            this.label_Email.TabIndex = 6;
+            this.label_Email.Text = "Email : ";
+            // 
+            // txt_Email
+            // 
+            this.txt_Email.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.txt_Email.Location = new System.Drawing.Point(671, 218);
+            this.txt_Email.Multiline = true;
+            this.txt_Email.Name = "txt_Email";
+            this.txt_Email.Size = new System.Drawing.Size(149, 24);
+            this.txt_Email.TabIndex = 7;
+            // 
+            // label_Sex
+            // 
+            this.label_Sex.AutoSize = true;
+            this.label_Sex.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Sex.Location = new System.Drawing.Point(88, 296);
+            this.label_Sex.Name = "label_Sex";
+            this.label_Sex.Size = new System.Drawing.Size(69, 25);
+            this.label_Sex.TabIndex = 8;
+            this.label_Sex.Text = "Sex : ";
+            // 
+            // label_Address
+            // 
+            this.label_Address.AutoSize = true;
+            this.label_Address.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Address.Location = new System.Drawing.Point(543, 292);
+            this.label_Address.Name = "label_Address";
+            this.label_Address.Size = new System.Drawing.Size(111, 25);
+            this.label_Address.TabIndex = 10;
+            this.label_Address.Text = "Address : ";
+            // 
+            // txt_Address
+            // 
+            this.txt_Address.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.txt_Address.Location = new System.Drawing.Point(671, 293);
+            this.txt_Address.Multiline = true;
+            this.txt_Address.Name = "txt_Address";
+            this.txt_Address.Size = new System.Drawing.Size(149, 24);
+            this.txt_Address.TabIndex = 11;
+            // 
+            // label_Branch
+            // 
+            this.label_Branch.AutoSize = true;
+            this.label_Branch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Branch.Location = new System.Drawing.Point(58, 375);
+            this.label_Branch.Name = "label_Branch";
+            this.label_Branch.Size = new System.Drawing.Size(99, 25);
+            this.label_Branch.TabIndex = 12;
+            this.label_Branch.Text = "Branch : ";
+            // 
+            // txt_Branch
+            // 
+            this.txt_Branch.Cursor = System.Windows.Forms.Cursors.No;
+            this.txt_Branch.Location = new System.Drawing.Point(163, 376);
+            this.txt_Branch.Multiline = true;
+            this.txt_Branch.Name = "txt_Branch";
+            this.txt_Branch.ReadOnly = true;
+            this.txt_Branch.Size = new System.Drawing.Size(149, 24);
+            this.txt_Branch.TabIndex = 13;
+            // 
+            // combox_Sex
+            // 
+            this.combox_Sex.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.combox_Sex.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.combox_Sex.FormattingEnabled = true;
+            this.combox_Sex.Items.AddRange(new object[] {
+            "Male",
+            "Female"});
+            this.combox_Sex.Location = new System.Drawing.Point(163, 296);
+            this.combox_Sex.Name = "combox_Sex";
+            this.combox_Sex.Size = new System.Drawing.Size(149, 24);
+            this.combox_Sex.TabIndex = 14;
+            // 
+            // label_Title
+            // 
+            this.label_Title.AutoSize = true;
+            this.label_Title.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Title.Location = new System.Drawing.Point(212, 9);
+            this.label_Title.Name = "label_Title";
+            this.label_Title.Size = new System.Drawing.Size(433, 44);
+            this.label_Title.TabIndex = 15;
+            this.label_Title.Text = "Customer Regestration";
+            // 
+            // btn_Submit
+            // 
+            this.btn_Submit.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Submit.Location = new System.Drawing.Point(548, 376);
+            this.btn_Submit.Name = "btn_Submit";
+            this.btn_Submit.Size = new System.Drawing.Size(123, 42);
+            this.btn_Submit.TabIndex = 16;
+            this.btn_Submit.Text = "Submit";
+            this.btn_Submit.UseVisualStyleBackColor = true;
+            this.btn_Submit.Click += new System.EventHandler(this.btn_Submit_Click);
+            // 
+            // btn_Clear
+            // 
+            this.btn_Clear.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Clear.Location = new System.Drawing.Point(714, 375);
+            this.btn_Clear.Name = "btn_Clear";
+            this.btn_Clear.Size = new System.Drawing.Size(123, 42);
+            this.btn_Clear.TabIndex = 17;
+            this.btn_Clear.Text = "Clear";
+            this.btn_Clear.UseVisualStyleBackColor = true;
+            this.btn_Clear.Click += new System.EventHandler(this.btn_Clear_Click);
+            // 
+            // CustomerRegister
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(849, 514);
+            this.Controls.Add(this.btn_Clear);
+            this.Controls.Add(this.btn_Submit);
+            this.Controls.Add(this.label_Title);
+            this.Controls.Add(this.combox_Sex);
+            this.Controls.Add(this.txt_Branch);
+            this.Controls.Add(this.label_Branch);
+            this.Controls.Add(this.txt_Address);
+            this.Controls.Add(this.label_Address);
+            this.Controls.Add(this.label_Sex);
+            this.Controls.Add(this.txt_Email);
+            this.Controls.Add(this.label_Email);
+            this.Controls.Add(this.txt_Phone);
+            this.Controls.Add(this.label_Phone);
+            this.Controls.Add(this.txt_Name);
+            this.Controls.Add(this.lable_Name);
+            this.Controls.Add(this.txt_CustomerId);
+            this.Controls.Add(this.customerId);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "CustomerRegister";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CustomerRegister";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label customerId;
+        private System.Windows.Forms.TextBox txt_CustomerId;
+        private System.Windows.Forms.Label lable_Name;
+        private System.Windows.Forms.TextBox txt_Name;
+        private System.Windows.Forms.Label label_Phone;
+        private System.Windows.Forms.TextBox txt_Phone;
+        private System.Windows.Forms.Label label_Email;
+        private System.Windows.Forms.TextBox txt_Email;
+        private System.Windows.Forms.Label label_Sex;
+        private System.Windows.Forms.Label label_Address;
+        private System.Windows.Forms.TextBox txt_Address;
+        private System.Windows.Forms.Label label_Branch;
+        private System.Windows.Forms.TextBox txt_Branch;
+        private System.Windows.Forms.ComboBox combox_Sex;
+        private System.Windows.Forms.Label label_Title;
+        private System.Windows.Forms.Button btn_Submit;
+        private System.Windows.Forms.Button btn_Clear;
+    }
+}
---
+++ Bank_management/CustomerRegister.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bank_management
+{
+    public partial class CustomerRegister : Form
+    {
+        public CustomerRegister()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Clear_Click(object sender, EventArgs e)
+        {
+            txt_Name.Text = " ";
+            txt_Address.Text = " ";
+            txt_Email.Text = " ";
+            txt_Phone.Text = " ";
+            combox_Sex.Text = " ";
+          
+        }
+
+        private void btn_Submit_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
