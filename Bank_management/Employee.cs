db60b478ac5abfb73f693034b5f44f6ac7112392
MD Maruf <maruf84602@gmail.com>
2026-04-28 19:44:39 +0600

    according to database

--- Bank_management/Employee.cs
+++ Bank_management/Employee.cs
@@ -32,8 +32,9 @@ namespace Bank_management
 
         }
 
-        public Employee(int userid,string name,string email,string position,string phone,double salary,string address,int branchId) : base(userid,name,phone,email,address)
+        public Employee(int userid,int employeeId,string name,string email,string position,string phone,double salary,string address,int branchId) : base(userid,name,phone,email,address)
         {
+            this.EmployeeId = employeeId;
             this.Position = position;
             this.Salary = salary;
             this.BranchId = branchId;
